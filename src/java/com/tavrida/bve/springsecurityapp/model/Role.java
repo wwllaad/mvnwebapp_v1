d71c59923d9801f293bf3fb8dfeb051a82a56c95
wwllaad <wwllaad@mail.ru>
2017-11-21 17:03:36 +0300

    bve_21_11_2017

---
+++ src/java/com/tavrida/bve/springsecurityapp/model/Role.java
@@ -0,0 +1,67 @@
+package com.tavrida.bve.springsecurityapp.model;
+/**
+ * Created by BVE on 21.11.2017.
+ * Simple JavaBean that represents roles of user.
+ */
+
+import org.springframework.security.core.userdetails.User;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
+
+    public Role(String name, Set<User> users) {
+        this.name = name;
+        this.users = users;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
+
+
+
